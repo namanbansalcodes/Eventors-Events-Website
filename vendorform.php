03e63a7fdce9f29d391988a9926c34552f102f17
Naman Bansal <namanbansalcodes@gmail.com>
2020-07-18 22:59:31 +0530

    Debug Form1

--- vendorform.php
+++ vendorform.php
@@ -1,13 +1,3 @@
 <?php
 
-if (isset($_POST["submit"])){
-    $name=$_POST["vname"];
-
-    $mailTo="webmanager@eventorsevents.com";
-    $subject="blah";
-    $headers="From: " .$name;
-    $text="This nigga be looking for you";
-
-    mail($mailTo,$subject,$headers,$text);
-    header("Location: index.php?mailsent");
-}
+echo "Hello";
