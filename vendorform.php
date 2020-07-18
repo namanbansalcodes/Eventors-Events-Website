b74df26985415058911ccaba9f16d82721e08c1e
Naman Bansal <namanbansalcodes@gmail.com>
2020-07-18 23:11:25 +0530

    Added Mailing code to  PHP file

--- vendorform.php
+++ vendorform.php
@@ -1,5 +1,15 @@
 <?php
 
-echo "Hello";
+if (isset($_POST["submit"])){
+    $name=$_POST["vname"];
+
+    $mailTo="webmanager@eventorsevents.com";
+    $subject="blah";
+    $headers="From: " .$name;
+    $text="This nigga be looking for you";
+
+    mail($mailTo,$subject,$headers,$text);
+    header("Location: index.php?mailsent");
+}
 
 ?>
