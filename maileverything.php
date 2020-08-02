ee4b3b97214d70537ba4cd9d982bc2f062e215b5
Naman Bansal <namanbansalcodes@gmail.com>
2020-08-02 15:22:34 +0530

    debug passed. Post data handling done

--- maileverything.php
+++ maileverything.php
@@ -1,7 +1,20 @@
 <?php
 
-parse_str($_POST['form'],$mailthis);
+parse_str($_POST['form'],$array);
 
-mail("namanb009@gmail.com",'this nigga lookin for u','Bitch!');
+$keys=array_keys($array);
+$size=sizeof($keys);
+
+$mailthis='';
+
+
+for($i=0;$i<$size;$i++){
+    $temp = $keys[$i].":  ".$array[$keys[$i]]."\n";
+    $mailthis=$mailthis.$temp;
+}
+
+mail("namanb009@gmail.com",'this nigga lookin for u',$mailthis);
+
+
 
 ?>
