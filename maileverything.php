18cb59561efc7c901325e3abdfad04ad60dee251
Naman Bansal <namanbansalcodes@gmail.com>
2020-08-02 15:28:55 +0530

    Debug 2: checking post request

--- maileverything.php
+++ maileverything.php
@@ -5,13 +5,8 @@ parse_str($_POST['form'],$array);
 $keys=array_keys($array);
 $size=sizeof($keys);
 
-$mailthis='';
+$mailthis=$array['vname'];
 
-
-for($i=0;$i<$size;$i++){
-    $temp = $keys[$i].":  ".$array[$keys[$i]]."\n";
-    $mailthis=$mailthis.$temp;
-}
 
 mail("namanb009@gmail.com",'this nigga lookin for u',$mailthis);
 
